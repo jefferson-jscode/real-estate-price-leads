fba5e65e2bc2e2837460aae24d2a79f97b253bc6
Jefferson Silva <jefferson.silva.maxnivel@gmail.com>
2022-08-02 18:45:10 -0300

    Initial commit

---
+++ real-estate-price-leads.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Real Estate Price Leads
+ * Description: Displays an input for the size of a property, a select with the states of the configured country and an input for the clients e-mail address. The client will receive the calculated property value based on selected state in his/her e-mail.
+ * Author: Jefferson Silva
+ * Author URI: https://github.com/jefferson-jscode/
+ */
